9d9daf2e11c221b8a5711161d6c67f742a17643b
dk-math <dk.gianluigi.1978@gmail.com>
2022-11-28 02:03:00 +0900

    second commit

---
+++ public/javascripts/index/app.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", () => {
+    let username = localStorage.getItem("username");
+    if (!username) {
+        username = window.prompt("What is your name?");
+        localStorage.setItem("username", username);
+    }
+
+    const tab = new Tab();
+    tab.setSwitchTabsEvent();
+})
