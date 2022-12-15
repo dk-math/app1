39be54b4c0856fb6e4f0a3d830a4e736ce6b1dcd
dk-math <dk.gianluigi.1978@gmail.com>
2022-12-15 19:13:19 +0900

    modified

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { Client } = require("pg");
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  const client = new Client({
-    user: 'daisuke_kondo',
-    host: '127.0.0.1',
-    database: 'app1db',
-    password: 'gianluigi1978',
-    port: 5432
-  });
-  client.connect();
-  client.query("SELECT * FROM member", (err, result) => {
-    content = result.rows;
-    var data = {
-      content: content
-    }
+    const data = {}
     res.render('index', data);
-    client.end();
-  });
 });
 
 module.exports = router;
