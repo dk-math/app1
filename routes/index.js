a1fd019ed4ad9d2a28d7edd00f958a5ca30a7acc
dk-math <dk.gianluigi.1978@gmail.com>
2022-12-15 17:43:33 +0900

    login routing

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,16 @@ var express = require('express');
 var router = express.Router();
 const { Client } = require("pg");
 
-var client = new Client({
-  user: 'daisuke_kondo',
-  host: '127.0.0.1',
-  database: 'app1db',
-  password: 'gianluigi1978',
-  port: 5432
-});
-client.connect();
-
 /* GET home page. */
 router.get('/', (req, res, next) => {
+  var client = new Client({
+    user: 'daisuke_kondo',
+    host: '127.0.0.1',
+    database: 'app1db',
+    password: 'gianluigi1978',
+    port: 5432
+  });
+  client.connect();
   client.query("SELECT * FROM member", (err, result) => {
     content = result.rows;
     var data = {
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const { Client } = require("pg");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,33 +10,38 @@ router.get('/', function(req, res, next) {
 router.get('/login', (req, res, next) => {
   var data = {
      title:'ログイン',
-     content:'名前とパスワードを入力下さい。'
+     content:'ユーザー名とパスワードを入力してください。'
   }
   res.render('users/login', data);
 });
 
 router.post('/login', (req, res, next) => {
-  db.User.findOne({
-    where:{
-      name:req.body.name,
-      pass:req.body.pass,
-    }
-  }).then(usr=>{
-    if (usr != null) {
-      req.session.login = usr;
+  var client = new Client({
+    user: 'daisuke_kondo',
+    host: '127.0.0.1',
+    database: 'app1db',
+    password: 'gianluigi1978',
+    port: 5432
+  });
+  client.connect();
+  client.query("SELECT * FROM member where name=req.body.name AND pass=req.body.pass", (err, result) => {
+    if (result != null) {
+      req.session.login = result;
       let back = req.session.back;
       if (back == null){
         back = '/';
       }
       res.redirect(back);
+      client.end();
     } else {
       var data = {
-        title:'Users/Login',
-        content:'名前かパスワードに問題があります。再度入力下さい。'
+        title:'ログイン',
+        content:'ユーザー名かパスワードに問題があります。再度入力してください。'
       }
       res.render('users/login', data);
+      client.end();
     }
-  })
+  });
 });
 
 module.exports = router;
