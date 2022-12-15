2f0350196af871fbd01192bf7eddf1a8a4602e87
dk-math <dk.gianluigi.1978@gmail.com>
2022-12-15 16:16:25 +0900

    postgresql connect

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const { Client } = require("pg");
+
+var client = new Client({
+  user: 'daisuke_kondo',
+  host: '127.0.0.1',
+  database: 'app1db',
+  password: 'gianluigi1978',
+  port: 5432
+});
+client.connect();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  client.query("SELECT * FROM member", (err, result) => {
+    content = result.rows;
+    var data = {
+      content: content
+    }
+    res.render('index', data);
+    client.end();
+  });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,13 +6,36 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/login', function(req, res, next) {
-  res.render('users/login', { title: 'Express' });
+router.get('/login', (req, res, next) => {
+  var data = {
+     title:'ログイン',
+     content:'名前とパスワードを入力下さい。'
+  }
+  res.render('users/login', data);
 });
 
-router.get('/login', function(req, res, next) {
-  res.render('users/login', { title: 'Express' });
+router.post('/login', (req, res, next) => {
+  db.User.findOne({
+    where:{
+      name:req.body.name,
+      pass:req.body.pass,
+    }
+  }).then(usr=>{
+    if (usr != null) {
+      req.session.login = usr;
+      let back = req.session.back;
+      if (back == null){
+        back = '/';
+      }
+      res.redirect(back);
+    } else {
+      var data = {
+        title:'Users/Login',
+        content:'名前かパスワードに問題があります。再度入力下さい。'
+      }
+      res.render('users/login', data);
+    }
+  })
 });
-
 
 module.exports = router;
